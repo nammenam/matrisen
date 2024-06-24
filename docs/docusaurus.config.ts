562af104291a5b4ad86265ce9b8f380e4784cd54
Brage <bragewiseth@icloud.com>
2024-06-24 11:18:49 +0200

    update docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kniv0gaffel', // Usually your GitHub org/user name.
   projectName: 'matrisen', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
